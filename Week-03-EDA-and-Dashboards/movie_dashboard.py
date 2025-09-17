efb29e5a06672c7a8bd28b53826bb8bd6c287a3f
Andrew Li <166180273+AndrewL05@users.noreply.github.com>
2025-09-17 03:28:25 +0000

    create movie dashboard

--- Week-03-EDA-and-Dashboards/movie_dashboard.py
+++ Week-03-EDA-and-Dashboards/movie_dashboard.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 st.set_page_config(page_title="MovieLens Dashboard", layout="wide")
 st.title("MovieLens Data Analysis Dashboard")
